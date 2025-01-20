9442541da526cb6ac5d9757d25ba152ea95e67ee
김민섭 <qwedsaszxc@naver.com>
2025-01-20 13:15:09 +0900

    250120-1314
    백엔드프로젝트생성

---
+++ back/furnhub/src/main/java/com/furnhub/FurnhubApplication.java
@@ -0,0 +1,13 @@
+package com.furnhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnhubApplication.class, args);
+	}
+
+}
---
+++ back/furnhub/src/test/java/com/furnhub/FurnhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furnhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
