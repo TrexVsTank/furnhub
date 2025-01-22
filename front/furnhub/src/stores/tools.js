93ee0b538d4535527da8e15f3cbc98192cca07fd
김민섭 <qwedsaszxc@naver.com>
2025-01-22 17:38:39 +0900

    250122-1738 피니아 이식 전

---
+++ front/furnhub/src/stores/tools.js
@@ -0,0 +1,15 @@
+// tools.js
+// Pinia Store (도구 상태 관리)
+
+import { defineStore } from "pinia";
+
+export const useToolStore = defineStore("tools", {
+  // state: () => ({
+  //   currentTool: "wall", // 현재 선택된 도구
+  // }),
+  // actions: {
+  //   setTool(tool) {
+  //     this.currentTool = tool;
+  //   },
+  // },
+});
